ddea76189229d9587b57014cd31dc81c8404816d
Thai Mai <jakehauer1234@gmail.com>
2024-11-24 21:00:56 +0700

    feat(database): integrate mongoose

---
+++ config/database.config.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to database succesfully");
+    } catch (error) {
+        console.log("Connected to database failled");
+        console.log("error");
+    }
+}
