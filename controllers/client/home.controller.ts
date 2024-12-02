2195bcc6737811060294b02ed99579af76e14d7b
Alien <minhhnghiii@gmail.com>
2024-12-02 15:40:32 +0700

    add songs on main pages

--- controllers/client/home.controller.ts
+++ controllers/client/home.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import Artist from "../../models/artist.model";
+import Song from "../../models/song.model";
 export const home = async (req: Request, res: Response) => {
   const artists = await Artist.find({ status: "active", deleted: false });
-  res.render("client/pages/home", { artists: artists });
+  const songs = await Song.find({ status: "active", deleted: false });
+  res.render("client/pages/home", { artists: artists, songs });
 };
