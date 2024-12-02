01067ae06285d683ea6b6282ce51461fa4f170af
Alien <minhhnghiii@gmail.com>
2024-12-02 23:13:37 +0700

    khi click bài hát thì play được bài đó bên aplayer

--- controllers/client/home.controller.ts
+++ controllers/client/home.controller.ts
@@ -4,5 +4,13 @@ import Song from "../../models/song.model";
 export const home = async (req: Request, res: Response) => {
   const artists = await Artist.find({ status: "active", deleted: false });
   const songs = await Song.find({ status: "active", deleted: false });
+  for (const song of songs) {
+    const artist = await Artist.findOne({
+      _id: song.artist,
+      status: "active",
+      deleted: false,
+    });
+    song["artist"] = artist?.fullName;
+  }
   res.render("client/pages/home", { artists: artists, songs });
 };
--- public/js/script.js
+++ public/js/script.js
@@ -1,17 +1,45 @@
 //Aplayer
 document.addEventListener("DOMContentLoaded", function () {
-    const aplayer = new APlayer({
-        container: document.getElementById('aplayer'),
-        audio: [
-            {
-                name: 'Qua Từng Khung Hình',
-                artist: 'Hustlang Robber & Ngắn',
-                url: '../songs/QUA TỪNG KHUNG HÌNH (Feat. Robber, Ngắn) - RAP VIỆT (youtube).mp3',
-                cover: 'https://lh3.googleusercontent.com/F2ywvCxiucFEM2CwP_6-3rYeoSUIhKPZO9rpMsKz8W3wDYE5SILCU7b9crM9GqwjmIy8TZtOPDqOJx1T=w60-h60-l90-rj',
-            }
-        ]
-    });
-    aplayer.play();
+  const aplayer = new APlayer({
+    container: document.getElementById("aplayer"),
+    audio: [
+      {
+        name: "Qua Từng Khung Hình",
+        artist: "Hustlang Robber & Ngắn",
+        url: "../songs/QUA TỪNG KHUNG HÌNH (Feat. Robber, Ngắn) - RAP VIỆT (youtube).mp3",
+        cover:
+          "https://lh3.googleusercontent.com/F2ywvCxiucFEM2CwP_6-3rYeoSUIhKPZO9rpMsKz8W3wDYE5SILCU7b9crM9GqwjmIy8TZtOPDqOJx1T=w60-h60-l90-rj",
+      },
+    ],
+  });
+  aplayer.play();
 });
 
 //End aplayer
+
+//Select song to play
+const songImage = document.querySelectorAll("[song-src]");
+if (songImage.length > 0) {
+  songImage.forEach((song) => {
+    song.addEventListener("click", () => {
+      const songSrc = song.getAttribute("song-src");
+      const songName = song.getAttribute("song-name");
+      const songArtist = song.getAttribute("song-artist");
+      const songCover = song.getAttribute("song-cover");
+
+      const aplayer = new APlayer({
+        container: document.getElementById("aplayer"),
+        audio: [
+          {
+            name: songName,
+            artist: songArtist,
+            url: songSrc,
+            cover: songCover,
+          },
+        ],
+      });
+      aplayer.play();
+    });
+  });
+}
+//End select song to play
