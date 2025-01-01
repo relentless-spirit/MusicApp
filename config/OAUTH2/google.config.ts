58766d820a1044397c2a1bd90218c92af5e3b00b
Alien Nguyen <129851755+minhnghi123@users.noreply.github.com>
2025-01-01 11:18:24 +0700

    Update google.config.ts

--- config/OAUTH2/google.config.ts
+++ config/OAUTH2/google.config.ts
@@ -14,7 +14,7 @@ export const googleConfig = () => {
       {
         clientID: GOOGLE_CLIENT_ID,
         clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/auth/google/callback",
+        callbackURL: "https://musicapp-fhfy.onrender.com/auth/google/callback",
       },
       async (_accessToken, _refreshToken, profile, done) => {
         try {
